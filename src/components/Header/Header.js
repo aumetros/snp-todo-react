48ba2a0a06d99464fbfa866e3f4a95a882509c60
aumetros <aum3tros@yandex.ru>
2023-09-14 20:04:45 +0300

    Add App: Add component Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Список дел</h1>
+    </header>
+  );
+}
+
+export default Header;
