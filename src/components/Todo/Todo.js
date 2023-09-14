67a1d2b7092be87ea291135b28cd9b037661cb5e
aumetros <aum3tros@yandex.ru>
2023-09-14 22:49:50 +0300

    Add App: Add component TodoList

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,12 +1,14 @@
 import "./Todo.css";
 import TodoForm from "../TodoForm/TodoForm";
 import Navbar from "../Navbar/Navbar";
+import TodoList from "../TodoList/TodoList";
 
 function Todo() {
   return (
   <div className="todo">
     <TodoForm />
     <Navbar />
+    <TodoList />
   </div>
   )
 }
---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,11 @@
+import "./TodoList.css";
+
+function TodoList() {
+  return (
+    <ul class="todo-list">
+
+    </ul>
+  )
+}
+
+export default TodoList;
