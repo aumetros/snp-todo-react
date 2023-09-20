92db5aed0d17dae75611c7b104ef4ba04ae6e4a2
aumetros <aum3tros@yandex.ru>
2023-09-20 22:45:31 +0300

    Add App: Add localStorage middleware

--- src/slices/index.js
+++ src/slices/index.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import tasksReducer from './tasksSlice.js';
 import filterReducer from './filtersSlice.js';
+import localStorageMiddleware from '../middlewares/localStorageMiddleware.js';
 
 export default configureStore({
   reducer: {
     tasks: tasksReducer,
     filters: filterReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(localStorageMiddleware),
 });
