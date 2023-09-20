7527b5244ab1ca6df3ae2a4f33a4611860fc70d2
aumetros <aum3tros@yandex.ru>
2023-09-20 23:05:43 +0300

    Add configureStore: Add preload localStorage

--- src/middlewares/localStorageMiddleware.js
+++ src/middlewares/localStorageMiddleware.js
@@ -1,7 +1,11 @@
-const localStorageMiddleware = (store) => (next) => (action) => {
+export const localStorageMiddleware = (store) => (next) => (action) => {
   const result = next(action);
   localStorage.setItem("todoState", JSON.stringify(store.getState()));
   return result;
 };
 
-export default localStorageMiddleware;
+export const preLoadStore = () => {
+  if (localStorage.getItem("todoState") !== null) {
+    return JSON.parse(localStorage.getItem("todoState"));
+  }
+};
--- src/slices/index.js
+++ src/slices/index.js
@@ -1,13 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
-import tasksReducer from './tasksSlice.js';
-import filterReducer from './filtersSlice.js';
-import localStorageMiddleware from '../middlewares/localStorageMiddleware.js';
+import { configureStore } from "@reduxjs/toolkit";
+import tasksReducer from "./tasksSlice.js";
+import filterReducer from "./filtersSlice.js";
+import {
+  localStorageMiddleware,
+  preLoadStore,
+} from "../middlewares/localStorageMiddleware.js";
 
 export default configureStore({
   reducer: {
     tasks: tasksReducer,
     filters: filterReducer,
   },
-  middleware: getDefaultMiddleware =>
+  preloadedState: preLoadStore(),
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(localStorageMiddleware),
 });
