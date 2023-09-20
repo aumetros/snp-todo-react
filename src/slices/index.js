923da55f2888bde83f4fe0caf85c8a3935d3886e
aumetros <aum3tros@yandex.ru>
2023-09-20 16:15:18 +0300

    Add Slices: Add filtersSlice

--- src/slices/index.js
+++ src/slices/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import tasksReducer from './tasksSlice.js';
+import filterReducer from './filtersSlice.js';
 
 export default configureStore({
   reducer: {
     tasks: tasksReducer,
+    filters: filterReducer,
   },
 });
