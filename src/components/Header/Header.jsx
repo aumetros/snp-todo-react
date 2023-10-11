4c3c15ebb0b85a8e96d428a0755cd947ddca3f97
aumetros <aum3tros@yandex.ru>
2023-10-11 22:33:51 +0300

    Refactor Header: Для стилизации компонента использован sass

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
-import "./Header.css";
+import styles from "./Header.module.scss"
 
 function Header() {
   return (
-    <header className="header">
-      <h1 className="header__title">Список дел</h1>
+    <header>
+      <h1 className={styles.title}>Список дел</h1>
     </header>
   );
 }
